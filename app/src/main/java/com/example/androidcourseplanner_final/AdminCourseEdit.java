9efd194e754203dbbb74547ee274ca47f3c597a7
GlassesNoGlasses <neilywang3@gmail.com>
2022-11-30 15:00:27 -0500

    Update AdminCourseEdit Fragment to display course information

--- app/src/main/java/com/example/androidcourseplanner_final/AdminCourseEdit.java
+++ app/src/main/java/com/example/androidcourseplanner_final/AdminCourseEdit.java
@@ -13,14 +13,18 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.androidcourseplanner_final.databinding.AdminCourseCreationBinding;
 
+import java.util.List;
+
 import Backend.Course;
 import Backend.CourseManager;
+import Backend.GetCoursesCallback;
 import Backend.LoginModel;
 import Backend.Session;
 
 public class AdminCourseEdit extends Fragment {
     private com.example.androidcourseplanner_final.databinding.AdminCourseEditBinding binding;
     private MainActivity view;
+    private String courseCode;
 
     @Override
     public View onCreateView(
@@ -28,7 +32,7 @@ public class AdminCourseEdit extends Fragment {
             Bundle savedInstanceState
     ) {
         view = new MainActivity();
-        Log.d("Course: ", view.getCourseCode());
+        courseCode = view.getCourseCode();
         binding = com.example.androidcourseplanner_final.databinding.AdminCourseEditBinding
                 .inflate(inflater, container, false);
         return binding.getRoot();
@@ -36,6 +40,16 @@ public class AdminCourseEdit extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        CourseManager.getInstance().getCourse(new GetCoursesCallback() {
+            @Override
+            public void onCallback(List<Course> courses) {
+                Course currentCourse = courses.get(0);
+                binding.courseCodeEntry.setText(currentCourse.getCourseCode());
+                binding.courseNameEntry.setText(currentCourse.getName());
+                setSessions(currentCourse.getSessions());
+            }
+        }, courseCode);
 
         binding.backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,6 +98,14 @@ public class AdminCourseEdit extends Fragment {
                         courseCode + " created successfully", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void setSessions(List<Session> sessions) {
+        for (Session s: sessions) {
+            if(s.equals(Session.Fall)) binding.fallCheckBox.setChecked(true);
+            else if(s.equals(Session.Summer)) binding.summerCheckBox.setChecked(true);
+            else if(s.equals(Session.Winter)) binding.winterCheckBox.setChecked(true);
+        }
     }
 
     @Override
