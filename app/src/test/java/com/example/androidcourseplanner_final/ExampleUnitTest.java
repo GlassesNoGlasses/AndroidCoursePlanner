07770a9a98f4add4ce54636c3edd0de003a72853
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2022-12-02 15:32:03 -0500

    Add mocks and presenter test

--- app/src/test/java/com/example/androidcourseplanner_final/ExampleUnitTest.java
+++ app/src/test/java/com/example/androidcourseplanner_final/ExampleUnitTest.java
@@ -3,9 +3,13 @@ package com.example.androidcourseplanner_final;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+
+import Backend.LoginModel;
+import Backend.LoginPresenter;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,7 +17,19 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+
+    @Mock
+    Login view;
+
+    @Mock
+    LoginModel model;
+
+    @Test
+    public void testPresenter() {
+
+    }
 
     @Test
     public void testCheckInfo() {
