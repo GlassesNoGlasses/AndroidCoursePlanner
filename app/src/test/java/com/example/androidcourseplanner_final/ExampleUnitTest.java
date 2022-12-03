1b2249c276fdff8f5a55c294ba9e95659eafe426
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2022-12-02 21:42:04 -0500

    Add presenter tests and add login methods

--- app/src/test/java/com/example/androidcourseplanner_final/ExampleUnitTest.java
+++ app/src/test/java/com/example/androidcourseplanner_final/ExampleUnitTest.java
@@ -5,11 +5,20 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import Backend.AuthenticationCallback;
+import Backend.GetProfileCallback;
 import Backend.LoginModel;
+import Backend.LoginModelAdapter;
 import Backend.LoginPresenter;
+import Backend.Profile;
+import Backend.Student;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -24,35 +33,34 @@ public class ExampleUnitTest {
     Login view;
 
     @Mock
-    LoginModel model;
+    LoginModelAdapter modelAdapter;
 
     @Test
     public void testPresenter() {
+        String email = "testUser@gmail.com";
+        String pass = "pass123";
+        when(view.getEmail()).thenReturn(email);
+        when(view.getPassword()).thenReturn(pass);
+        when(modelAdapter.model.getFirebaseAuth().signInWithEmailAndPassword(email, pass)).thenReturn(null);
+        LoginPresenter presenter = new LoginPresenter(view, modelAdapter.model);
+        presenter.signIn(email, pass, new AuthenticationCallback() {
+            @Override
+            public void onSuccess() {
+                //Do nothing, let view take care of execution
+            }
+            @Override
+            public void onFailure() {
+                //Do nothing, let view take care of execution
+            }
+        });
+        Mockito.verify(view).generateMessage("Invalid Login Information");
 
     }
-
     @Test
-    public void testCheckInfo() {
-        //To be completed
+    public void testGetEmail() {
+        String email = "testUser@gmail.com";
+        when(view.getEmail()).thenReturn(email);
+        assertEquals(view.getEmail(), email);
     }
 
-    @Test
-    public void testSignUp() {
-        // To be completed
-    }
-
-    @Test
-    public void testSignIn() {
-        // To be completed
-    }
-
-    @Test
-    public void testIsSignedIn() {
-        // To be completed
-    }
-
-    @Test
-    public void testGetProfile() {
-        // To be completed
-    }
 }
